583b624f411d2b63a5f251ddbb6746954027b1d7
Kofusu <kofusu@protonmail.com>
2025-05-15 09:31:04 +0700

    feat: adding axiosInstance interceptors response

--- app/_lib/configs/axios/axiosInstance/axiosInstance.tsx
+++ app/_lib/configs/axios/axiosInstance/axiosInstance.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
 	baseURL: '',
 });
+
+// Executor before request
 axiosInstance.interceptors.request.use(
 	(config) => {
 		if (!config.url) throw new Error('Missing API URL');
@@ -10,6 +12,15 @@ axiosInstance.interceptors.request.use(
 		// const token = getToken();
 		// if (token) config.headers.Authorization = `Bearer ${token}`;
 		return config;
+	},
+	(error) => Promise.reject(error),
+);
+
+// Executor after get response
+axiosInstance.interceptors.response.use(
+	(response) => {
+		if (!response.status) throw new Error('No Status Response');
+		return response;
 	},
 	(error) => Promise.reject(error),
 );
